373ceb90e0557a4233c1d6f1c284870a1e1376d2
sergiocohens <sergiocohensalama@pursuit.org>
2020-01-19 11:07:04 -0500

    erased-template-set-nav-bar-and-created-first-components

---
+++ unit4/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { BrowserRouter, Route, Link } from "react-router-dom"
+import Home from './Components/Home'
+import About from './Components/About'
+import './App.css'
+
+class App extends React.Component {
+  constructor(){
+    super()
+  }
+  render() {
+    return (
+      <div className="App">
+        <nav>
+          <Link to="/">Home</Link>
+          {"  "}
+          <Link to="/about">About</Link>
+        </nav>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ unit4/src/secrets.js
@@ -0,0 +1,2 @@
+const API_KEY = 'AIzaSyAnq5tbTaIO5vrfzAe1-h8cGeg03E00ArQ'
+export default API_KEY
