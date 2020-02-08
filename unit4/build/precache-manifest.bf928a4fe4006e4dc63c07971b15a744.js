0e47d323146367953ea33b6f3810d7e88d112a04
sergiocohens <sergiocohensalama@pursuit.org>
2020-02-07 22:30:46 -0500

    build and changed gitignore

---
+++ unit4/build/precache-manifest.bf928a4fe4006e4dc63c07971b15a744.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd72d52707866f08467568d11d3ea2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42e636cd1233f717e0fc",
+    "url": "/static/css/main.84c2a490.chunk.css"
+  },
+  {
+    "revision": "8187c178ea1299df8d87",
+    "url": "/static/js/2.204d5120.chunk.js"
+  },
+  {
+    "revision": "adf315857185901d18c2339ec58b6b66",
+    "url": "/static/js/2.204d5120.chunk.js.LICENSE"
+  },
+  {
+    "revision": "42e636cd1233f717e0fc",
+    "url": "/static/js/main.285a80ea.chunk.js"
+  },
+  {
+    "revision": "738e52f343837859d94b",
+    "url": "/static/js/runtime-main.7af8ac33.js"
+  }
+]);
